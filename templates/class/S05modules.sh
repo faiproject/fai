269c7e9acbb6163bc8bb898fee379c534640b0bb
Thomas Lange <lange@informatik.uni-koeln.de>
2000-11-14 18:12:23 +0000

    use all classes for loading modules defined before S05

--- templates/class/S05modules.sh
+++ templates/class/S05modules.sh
@@ -2,8 +2,10 @@
 
 # load kernel modules
 # message of loading modules are written to syslogd and read with dmesg
+# for all classes, that are defines before this script is called, lokk for
+# a $class.mod file and execute it.
 
-conffiles="global $HOSTNAME"
+conffiles="global $classes $HOSTNAME"
 
 for file in $conffiles ; do
     if [ -f "${file}.mod" ]; then
