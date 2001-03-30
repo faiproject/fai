58bcc688e564ce93be994370aab87ae95a8a8e59
Thomas Lange <lange@informatik.uni-koeln.de>
2001-03-30 12:37:22 +0000

    automaticly add class for a subnet

--- templates/class/S01alias.sh
+++ templates/class/S01alias.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 
 # all faiclient's are using classes in faiclient
-
 case $HOSTNAME in
 
     faiclient??)
-	cat faiclient
-	;;
+	cat faiclient ;;
+esac
 
+
+# if host belongs to class C subnet 134.95.9.0 use class NET_9
+case $IPADDR in
+    134.95.9.*)
+	echo NET_9 ;;
 esac
