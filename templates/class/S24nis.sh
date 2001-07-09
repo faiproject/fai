543b4f474e797b6040ab251f3c386d7df5ddf661
Thomas Lange <lange@informatik.uni-koeln.de>
2001-07-09 14:45:57 +0000

    merge two echo commands to one

--- templates/class/S24nis.sh
+++ templates/class/S24nis.sh
@@ -3,8 +3,7 @@
 # add NIS if YPDOMAIN is defined
 
 if [ -n "$YPDOMAIN" ];then
-   echo NIS
-   echo $YPDOMAIN | tr '.a-z-' '_A-Z_'
+   echo "NIS $YPDOMAIN" | tr '.a-z-' '_A-Z_'
 else
    echo NONIS
 fi
