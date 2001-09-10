811b5ee495b57c308e609aa4c62cf95ca5ec9932
Thomas Lange <lange@informatik.uni-koeln.de>
2001-09-10 16:56:04 +0000

    rearrange of code, use less lines

--- templates/class/S01alias.sh
+++ templates/class/S01alias.sh
@@ -2,25 +2,20 @@
 
 # all hosts named ant?? are using the classes in file anthill
 case $HOSTNAME in
-    ant??)
-	cat anthill ;;
+    ant??) cat anthill ;;
 esac
 
 
 # the Beowulf cluster; all nodes except the master node use classes from file class/atoms
 case $HOSTNAME in
     atom00) ;;
-
-    atom??)
-        cat atoms ;;
+    atom??) cat atoms ;;
 esac
 
 # if host belongs to class C subnet 134.95.9.0 use class NET_9
 case $IPADDR in
-    134.95.9.*)
-	echo NET_9 ;;
+    134.95.9.*)	echo NET_9 ;;
 esac
 
-
-# echo architecture
+# echo architecture in upper case
 dpkg --print-installation-architecture | tr /a-z/ /A-Z/
