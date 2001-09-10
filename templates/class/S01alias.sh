368f8432c32f18cd8601598a534d914ec09d0c60
Thomas Lange <lange@informatik.uni-koeln.de>
2001-09-10 16:02:56 +0000

    add examples for Beowulf cluster

--- templates/class/S01alias.sh
+++ templates/class/S01alias.sh
@@ -2,11 +2,18 @@
 
 # all hosts named ant?? are using the classes in file anthill
 case $HOSTNAME in
-
     ant??)
 	cat anthill ;;
 esac
 
+
+# the Beowulf cluster; all nodes except the master node use classes from file class/atoms
+case $HOSTNAME in
+    atom00) ;;
+
+    atom??)
+        cat atoms ;;
+esac
 
 # if host belongs to class C subnet 134.95.9.0 use class NET_9
 case $IPADDR in
