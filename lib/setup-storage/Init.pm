5bdc1ee23f7c4c080f25f3bec4cfdc8be530462f
Thomas Lange <lange@informatik.uni-koeln.de>
2008-09-06 14:31:35 +0000

    fix URL for faiwiki

--- lib/setup-storage/Init.pm
+++ lib/setup-storage/Init.pm
@@ -27,7 +27,7 @@ use strict;
 # @brief Initialize all variables and acquire the set of disks of the system.
 #
 # The layout of the data structures is documented in the wiki:
-# http://faiwiki.debian.net/index.php/Setup-storage
+# http://faiwiki.informatik.uni-koeln.de/index.php/Setup-storage
 #
 # $Id$
 #
