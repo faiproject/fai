a63b2344980fa38e75844a15656b114ebc875d1e
Thomas Lange <lange@informatik.uni-koeln.de>
2010-07-15 09:14:38 +0000

    mkramdisk,task_sysinfo,updatebase,fai-do-scripts,fai-debconf, faimond-gui,setup-storage,Commands.pm,Exec.pm,Fstab.pm,Init.pm, Parser.pm,Sizes.pm,Volumes.pm: add $Id$

--- lib/setup-storage/Parser.pm
+++ lib/setup-storage/Parser.pm
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 
+# $Id$
 #*********************************************************************
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
