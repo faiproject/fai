27b8e7c15f793275fa7d0e23d6f2e0b12e6fc348
Kerim Gueney <kgueney@uni-koeln.de>
2015-07-03 11:43:55 +0200

    Abort installation in case of a non-boot efi partition

--- lib/setup-storage/Commands.pm
+++ lib/setup-storage/Commands.pm
@@ -1324,7 +1324,13 @@ sub setup_partitions {
     &FAI::push_command($cmd, "prep2_$dn", "prep3_$dn");
 
     my $efi_cmd = "true";
-    $efi_cmd = "parted $disk set $part_id bios_grub on" if ($part->{efi} == 1 and $part->{mountpoint} =~ m/^\/boot/);
+
+    die "Partition type efi requires mountpoint at or in /boot"
+      if ($part->{efi} == 1 and !($part->{mountpoint} =~ m/^\/boot/));
+
+    $efi_cmd = "parted $disk set $part_id bios_grub on"
+      if ($part->{efi} == 1 and $part->{mountpoint} =~ m/^\/boot/);
+
     &FAI::push_command($efi_cmd, "prep3_$dn", "exist_$dn");
 
 
