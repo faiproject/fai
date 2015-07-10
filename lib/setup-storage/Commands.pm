320200632dd6f5cc85940aacbed0ffc3a6ea010c
Thomas Lange <lange@informatik.uni-koeln.de>
2015-07-10 11:56:02 +0200

    Revert "Create a separate command scalar for sgdisk to prevent any potential collisions with the iosetup command"
    This reverts commit d46568148df5b5f11d7a4a181f9c81ad9af6c0c5.

--- lib/setup-storage/Commands.pm
+++ lib/setup-storage/Commands.pm
@@ -1321,12 +1321,8 @@ sub setup_partitions {
       $pre, "prep2_$dn");
     my $cmd = "true";
     $cmd = "losetup -o $start $dn $disk" if ((&FAI::loopback_dev($disk))[0]);
-    &FAI::push_command($cmd, "prep2_$dn", "prep3_$dn");
-
-    my $efi_cmd = "true";
-    $efi_cmd = "sgdisk --typecode=$part_id:ef00 $disk" if ($part->{efi} == 1);
-    &FAI::push_command($efi_cmd, "prep3_$dn", "exist_$dn");
-
+    $cmd = "sgdisk --typecode=$part_id:ef00 $disk" if ($cmd eq "true" and $part->{efi} == 1);
+    &FAI::push_command($cmd, "prep2_$dn", "exist_$dn");
 
     # (re-)set all flags
     my $flags = "";
