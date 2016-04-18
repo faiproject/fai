eabd98790b6293fee890421081e6214fed7fc060
andrew bezella <abezella@archive.org>
2016-04-18 12:24:37 -0700

    do not echo hostname to /proc in dirinstall as this changes the hostname of the host system

--- examples/simple/hooks/setup.DEFAULT.sh
+++ examples/simple/hooks/setup.DEFAULT.sh
@@ -2,4 +2,10 @@
 
 # use short hostname instead of FQDN
 export HOSTNAME=${HOSTNAME%%.*}
-echo $HOSTNAME > /proc/sys/kernel/hostname
+# n.b. use $action instead of $FAI_ACTION
+# as the latter is apparently unset at this point in dirinstall
+if [ "$action" = "dirinstall" ] ; then
+  :
+else
+  echo $HOSTNAME > /proc/sys/kernel/hostname
+fi
