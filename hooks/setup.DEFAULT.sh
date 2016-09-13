bbe2dbe928e862deac26c03a37a9faa895ee5403
Thomas Lange <lange@informatik.uni-koeln.de>
2016-09-13 16:36:54 +0200

    set hostname only when installing on host

--- hooks/setup.DEFAULT.sh
+++ hooks/setup.DEFAULT.sh
@@ -5,5 +5,5 @@ export HOSTNAME=${HOSTNAME%%.*}
 # n.b. use $action instead of $FAI_ACTION
 # as the latter is apparently unset at this point in dirinstall
 if [ $do_init_tasks -eq 1 ]; then
-    echo $HOSTNAME > /proc/sys/kernel/hostname
+  echo $HOSTNAME > /proc/sys/kernel/hostname
 fi
