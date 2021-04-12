a02a084073bd1bf057bfb8572d0b30f934631b8b
Thomas Lange <lange@debian.org>
2021-04-12 22:05:24 +0200

    add Thomas

--- lib/setup-storage/Volumes.pm
+++ lib/setup-storage/Volumes.pm
@@ -26,7 +26,7 @@ use strict;
 #
 # @brief Parse the current partition table and LVM/RAID configurations
 #
-# @author Christian Kern, Michael Tautschnig
+# @author Christian Kern, Michael Tautschnig, Thomas Lange
 # @date Sun Jul 23 16:09:36 CEST 2006
 #
 ################################################################################
