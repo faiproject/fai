ae22d02f7bb035439f0b70a1887e818ac8d293b7
Thomas Lange <lange@debian.org>
2022-10-06 19:07:38 +0200

    delete trailing space

--- lib/dracut/80fai-autodiscover/module-setup.sh
+++ lib/dracut/80fai-autodiscover/module-setup.sh
@@ -13,7 +13,7 @@ depends() {
 install() {
 
     inst_multiple nmap grep cut sort uniq head nc dialog getent
-    
+
     inst_hook cmdline  40 "$moddir/parse-fai.sh"
     inst_hook initqueue/online 95 "$moddir/fai-scan.sh"
 }
